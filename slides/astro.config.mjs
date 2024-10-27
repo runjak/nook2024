30da92de7958ec3b778c879a6881ec9407a10c8c
Jakob Runge <github@runjak.codes>
2024-10-27 11:11:56 +0100

    add ViewTransitions to Layout

--- slides/astro.config.mjs
+++ slides/astro.config.mjs
@@ -2,4 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    prefetch: {
+        /**
+         * Enabling prefetching for ViewTransitions.
+         *
+         * https://docs.astro.build/en/reference/configuration-reference/#prefetchprefetchall
+         * https://docs.astro.build/en/guides/view-transitions
+         */
+        prefetchAll: true
+    }
+});
