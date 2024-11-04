ca3585ad448cfb2292ea6517cf8b04f3d56e59c0
Jakob Runge <github@runjak.codes>
2024-11-04 09:48:36 +0100

    use uv as farbverlauf example

--- slides/src/glsl/03-beispiele-farbverlauf.glsl
+++ slides/src/glsl/03-beispiele-farbverlauf.glsl
@@ -2,11 +2,8 @@ precision mediump float;
 
 uniform vec3 iResolution;
 
-const vec4 color1 = normalize(vec4(0, 35, 34, 255)); // #002322
-const vec4 color2 = normalize(vec4(0, 99, 96, 255)); // #006360
-
 void main() {
-  vec2 relCoord = gl_FragCoord.xy / iResolution.xy;
+  vec2 uv = gl_FragCoord.xy / iResolution.xy;
 
-  gl_FragColor = mix(color1, color2, relCoord.y);
+  gl_FragColor = vec4(uv.xy, 0.0, 1.0);
 }
