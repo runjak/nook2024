ab16765393c6583610de3579ccf764add2b825d0
Jakob Runge <github@runjak.codes>
2024-11-04 13:11:08 +0100

    adjust position example

--- slides/src/glsl/03-beispiele-position.glsl
+++ slides/src/glsl/03-beispiele-position.glsl
@@ -11,12 +11,8 @@ void main() {
     vec2 uv = gl_FragCoord.xy / iResolution.xy;
     vec2 uvMouse = iMouse.xy / iResolution.xy;
 
-    float deltaMouse = distance(gl_FragCoord.xy, iMouse.xy) / 100.0;
-    float clampedDeltaMouse = clamp(deltaMouse, 0.0, 1.0);
+    float deltaMouse = distance(uv, uvMouse);
+    float clampedDeltaMouse = clamp(deltaMouse * 10.0, 0.0, 1.0);
 
-    float t = iMouse.x / iResolution.x;
-
-    // float lineAShape = smoothstep(1.0 - clamp(distance(deltaMouse, 0.5), 0.0, 1.0), 1.0, clampedDeltaMouse);
-
-    gl_FragColor = vec4(mix(color3, color2, clampedDeltaMouse));
+    gl_FragColor = vec4(mix(color3, color1, clampedDeltaMouse));
 }
